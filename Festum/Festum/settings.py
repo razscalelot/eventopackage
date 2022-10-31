c21d9519a069ba7d6b8995cbb727ffb6a79e506d
razscalelot <you@example.com>
2022-10-31 12:19:25 +0530

    change database username and password

--- Festum/Festum/settings.py
+++ Festum/Festum/settings.py
@@ -136,9 +136,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'eventopackage',
-        'USER': 'ubuntu',
-        'PASSWORD': '',
-        'HOST': 'localhost',
+        'USER': 'evento',
+        'PASSWORD': 'M$%Z5LcbzwsA',
+        'HOST': 'db.festumevento.com',
         'PORT': '3306',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
