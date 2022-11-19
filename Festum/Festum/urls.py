a5d968a7579e3af760a18627f943ec7d16c0422e
razscalelot <you@example.com>
2022-11-19 14:57:33 +0530

    static and media change

--- Festum/Festum/urls.py
+++ Festum/Festum/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.conf.urls import url,include
 
@@ -23,4 +25,4 @@ from django.conf.urls import url,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     url(r'api/',include('userApi.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
